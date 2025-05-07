98a16df28738f89402a888ecdc05278a2aa68d9e
wachxra <wachirawadee.junt@bumail.net>
2025-05-07 12:46:58 +0700

    Can use end credit scene
    - But the end credit after player died always bug and not do anything. Will be fix later

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("UI Panels")]
     public GameObject howToPlayPanel;
     public GameObject deathPanel;
+
+    [Header("Sound Settings")]
+    [SerializeField] private AudioSource backgroundMusic;
+    [SerializeField] private AudioSource clickSound;
+
+    private bool isInEndCreditScene = false;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        if (SceneManager.GetActiveScene().name == "EndCredit")
+        {
+            isInEndCreditScene = true;
+        }
+
+        if (backgroundMusic != null && !backgroundMusic.isPlaying)
+        {
+            backgroundMusic.loop = true;
+            backgroundMusic.Play();
+        }
+    }
+
+    void Update()
+    {
+        if (isInEndCreditScene && Input.anyKeyDown)
+        {
+            StartCoroutine(PlayClickAndBackToMainMenu());
+        }
+    }
 
     public void StartGame()
     {
         ResetGameStatus();
         Time.timeScale = 1f;
-        SceneManager.LoadScene("TutorialScene");
+        StartCoroutine(PlayClickAndLoadScene("TutorialScene"));
     }
 
     public void TryAgain()
@@ -22,23 +55,42 @@ public class MainMenu : MonoBehaviour
 
         ResetGameStatus();
         Time.timeScale = 1f;
+
+        if (clickSound != null)
+        {
+            clickSound.Play();
+            Invoke(nameof(ReloadCurrentScene), clickSound.clip.length);
+        }
+        else
+        {
+            ReloadCurrentScene();
+        }
+    }
+
+    private void ReloadCurrentScene()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
     public void ExitGame()
     {
+        if (clickSound != null)
+        {
+            clickSound.Play();
+        }
+
         Application.Quit();
     }
 
     public void GoToCreditScene()
     {
-        SceneManager.LoadScene("EndCredit");
+        StartCoroutine(PlayClickAndLoadScene("EndCredit"));
     }
 
     public void BackToMainMenu()
     {
         ResetGameStatus();
-        SceneManager.LoadScene("MainMenu");
+        StartCoroutine(PlayClickAndLoadScene("MainMenu"));
     }
 
     public void ShowHowToPlay()
@@ -59,5 +111,26 @@ public class MainMenu : MonoBehaviour
             Destroy(instance.gameObject);
             PlayerStatusManager.instance = null;
         }
+    }
+
+    private IEnumerator PlayClickAndLoadScene(string sceneName)
+    {
+        if (clickSound != null)
+        {
+            clickSound.Play();
+            yield return new WaitForSeconds(clickSound.clip.length);
+        }
+        SceneManager.LoadScene(sceneName);
+    }
+
+    private IEnumerator PlayClickAndBackToMainMenu()
+    {
+        if (clickSound != null)
+        {
+            clickSound.Play();
+            yield return new WaitForSeconds(clickSound.clip.length);
+        }
+        ResetGameStatus();
+        SceneManager.LoadScene("MainMenu");
     }
 }
